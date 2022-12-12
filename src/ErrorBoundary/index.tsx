29f00f541f144682d27b70afc08f4cef731ad0a2
Maryna Darashuk <m.doroshuk@bk.ru>
2022-12-12 09:59:23 +0300

    refactor: change Error Boundary prop types

---
+++ src/ErrorBoundary/index.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { Container } from './styled';
+
+interface IError {
+  children: JSX.Element | JSX.Element[];
+}
+
+interface IState {
+  hasError?: boolean;
+}
+
+class ErrorBoundary extends React.Component<IError> {
+  constructor(props: IError) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  static getDerivedStateFromError() {
+    return { hasError: true };
+  }
+
+  render() {
+    const { hasError }: IState = this.state;
+    const { children } = this.props;
+    if (hasError) {
+      return (
+        <Container>
+          Oops... Something went wrong :(
+        </Container>
+      );
+    }
+
+    return children;
+  }
+}
+
+export default ErrorBoundary;
